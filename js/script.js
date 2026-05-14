8779afdaaacda0ec96898b2b871709fd414a1b71
guzmangit <ricardocruzguzman6@gmail.com>
2026-05-14 13:30:45 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function saludarVisitante() {
+    let nombre = prompt("¿Cómo te llamas?");
+    if (nombre != null && nombre != "") {
+        alert("¡Hola " + nombre + "! Bienvenido a los Rincones de Huajolotitlán.");
+    }
+}
